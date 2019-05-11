1ef270354cdafd33c10ba792bd450a62284b0087
Firman Abdul Hakim <fimzhakim@gmail.com>
2019-05-11 22:40:30 +0700

    add readme

--- dd_helper.php
+++ dd_helper.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * dd function helper like laravel
+ *
+ * PHP version 5 or Higher
+ *
+ * @category Helper
+ * @author   Firman Abdul Hakim @firmanjs
+ * @license  http://opensource.org/licenses/BSD-3-Clause 3-clause BSD
+ * @link
+ */
 function dd($var){
   echo "<pre>";
   print_r($var);
